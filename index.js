178340b5c5a01cbfb52ab3f78ffe5bae9f556b25
embed-Rayn <57181674+embed-Rayn@users.noreply.github.com>
2020-10-23 14:57:45 +0900

    add router

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { nextTick } from "process";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 // const express = require("express");
 const app = express();
 
-const PORT = 4000;
+const PORT = 4001;
 
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
@@ -11,12 +14,17 @@ const handleHome = (req, res) => res.send("Hello from home");
 
 const handleProfile = (req, res) => res.send("You are in my profile");
 
-const betweenHome = (req, res, next) => {
-    console.log("I'm between");
-    next();
-}
-
-app.use(betweenHome);
+// middle ware sample
+// const betweenHome = (req, res, next) => {
+//     console.log("I'm between");
+//     next();
+// }
+// app.use(betweenHome)
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
---
+++ init.js
@@ -0,0 +1,6 @@
+import app from "./app";
+
+const PORT = 4002;
+const handlListening = () => console.log(`✔ Listening on: http://localhost${PORT}`);
+
+app.listen(PORT, handlListening);
