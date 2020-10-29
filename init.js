0898b65038c758c109db6d00644ded2ff555d9b8
embed-Rayn <57181674+embed-Rayn@users.noreply.github.com>
2020-10-29 13:38:39 +0900

    update edit-profile

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 import app from "./app";
 
 const PORT = 4002;
-const handlListening = () => console.log(`✔ Listening on: http://localhost${PORT}`);
+const handlListening = () => console.log(`✔ Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handlListening);
